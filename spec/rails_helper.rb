c74ebd3c4ca6149d0bcd1645ebd85f30d188b0b7
Austin Mason <austinpmason@gmail.com>
2020-06-18 23:34:18 +0100

    add selenium rspec setup for system tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,8 +16,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
-require 'capybara-screenshot/rspec'
-require_relative 'support/factory_bot'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |file| require file }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
