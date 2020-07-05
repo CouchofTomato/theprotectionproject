80e71cfbd2cc2f323d8ba74ade7cd357652ad4a6
Austin Mason <austinpmason@gmail.com>
2020-07-05 21:55:35 +0100

    Add user factories
    The standard user factory uses faker for the email and password and sets
    admin to false, the admin_user factory inherits from the user factory
    but sets admin to true

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    admin { false }
+  end
+
+  factory :admin_user do
+    admin { true }
   end
 end
