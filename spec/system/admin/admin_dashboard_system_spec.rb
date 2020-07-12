77b60e971fb00381df113cd67479f13a44206de8
Austin Mason <austinpmason@gmail.com>
2020-07-12 21:44:13 +0100

    add ApplicationController as parent controller for rails_admin

--- spec/system/admin/admin_dashboard_system_spec.rb
+++ spec/system/admin/admin_dashboard_system_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'navgiating to /admin', type: :system do
 
   it 'cannot be acccessed by a non-admin user' do
     user = create(:user)
-    login_as(user, scope: user)
+    login_as(user, scope: :user)
 
     visit rails_admin_path
     expect(page).to have_current_path(root_path)
