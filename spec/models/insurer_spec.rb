f856f11a793de1ad540edb96ed37b708167ace91
Austin Mason <austinpmason@gmail.com>
2020-07-15 22:28:07 +0100

    Feature: Insurer Validations
    Because I want insurers to be be unique
    
    This commit: Adds a validation to the insurer model that the name is
    present and also unique

--- spec/models/insurer_spec.rb
+++ spec/models/insurer_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Insurer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { create :insurer }
+
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 end
