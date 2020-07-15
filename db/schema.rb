8087b0e9475b16c81768c6b3793cd31943235ff6
Austin <austinpmason@gmail.com>
2020-07-15 22:44:48 +0100

    Add insurers (#11)
    * Create Insurer model
    
    The insurer model represents an insurance company and has a name
    attribute. The name can't be null and must be unique
    
    * Feature: Insurer Validations
    
    Because I want insurers to be be unique
    
    This commit: Adds a validation to the insurer model that the name is
    present and also unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_223721) do
+ActiveRecord::Schema.define(version: 2020_07_15_210102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "insurers", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_insurers_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
