b1cbcbcac0fe2d1a09b4bd93631af16fb6314717
Austin Mason <austinpmason@gmail.com>
2020-07-16 22:38:41 +0100

    Add validation for insurer presence to product model
    Because: I don't want a product to be created without an insurer
    
    This commit: Adds a validation for the presence of an insurer to the
    product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :insurer
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :insurer, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Product, type: :model do
   it { is_expected.to belong_to :insurer }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  it { is_expected.to validate_presence_of :insurer }
 end
