e9f6d94ce98e8c999efa5ca8c833db27fae568a8
Austin Mason <austinpmason@gmail.com>
2020-07-22 21:55:34 +0100

    ProductModule validations
    Because: I want the ProductModule to validate attributes
    
    This commit: Makes the following validation
    * Validates presence of name
    * Validates uniqueness of name scoped to category
    * validates presence of category
    * validates inclusion of category in a CATEGORIES array constant
    * validates presence of sum_assured
    * validates sum_assured is in a valid format

--- app/models/product_module.rb
+++ app/models/product_module.rb
@@ -1,3 +1,25 @@
 class ProductModule < ApplicationRecord
+  CATEGORIES =
+    %w[
+      core
+      outpatient
+      medicines_and_appliances
+      wellness
+      maternity
+      dental_and_optical
+      evacuation_and_repatriation
+    ].freeze
+  VALID_CURRENCY = /
+                    \A(eur|gbp|usd)\s?\d+,?\d*,?\d*\s?\|?\s?
+                    (eur|gbp|usd)\s?\d+,?\d*,?\d*\s?\|?\s?
+                    (eur|gbp|usd)\s?\d+,?\d*,?\d*\z
+                   /ix.freeze
+
   belongs_to :product
+  validates :name, presence: true, uniqueness: { scope: :category, case_sensitive: false }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  validates :sum_assured,
+            presence: true,
+            format: { with: VALID_CURRENCY,
+                      message: 'Please write the sum assured in the format "USD X,XXX,XXX | GBP X,XXX,XXX | EUR X,XXX,XXX"' }
 end
--- spec/factories/product_modules.rb
+++ spec/factories/product_modules.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { 'Gold' }
     category { 'core' }
     sum_assured { 'USD 3,000,000 | EUR 3,200,000 | GBP 2,500,000' }
-    product { nil }
+    product
   end
 end
