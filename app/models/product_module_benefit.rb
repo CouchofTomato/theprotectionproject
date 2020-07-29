342b36b88914a7a0fb54f65ff5eb77636aa434f8
Austin Mason <austinpmason@gmail.com>
2020-07-29 23:10:56 +0100

    Set ProductModuleBenefit validations
    Because: I want my product modules to be validated
    
    This commit: Sets presence validations on benefit_limit and
    benefit_status and a uniqueness validation on product_module_id scoped
    to benefit_id so that a product_module and benefit can only be set once
    since it should not be possible to have a module with multiple benefits
    that are the same.

--- app/models/product_module_benefit.rb
+++ app/models/product_module_benefit.rb
@@ -1,4 +1,10 @@
 class ProductModuleBenefit < ApplicationRecord
   belongs_to :product_module
   belongs_to :benefit
+
+  enum benefit_status: { 'paid in full' => 0, 'capped benefit' => 1 }
+
+  validates :benefit_status, presence: true
+  validates :benefit_limit, presence: true
+  validates :product_module_id, uniqueness: { scope: :benefit_id }
 end
