cab129c5d1f2175bcab668a6bd6b8661689d9de9
Austin Mason <austinpmason@gmail.com>
2020-08-03 22:27:13 +0100

    Delete product modules when associated product is deleted
    Because: I want product modules to be deleted when the associated
    product is deleted
    
    This commit: Adds a has_many assocation on products for product modules
    with the dependent: :destroy option set to true

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :insurer
+  has_many :product_modules, dependent: :destroy
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :insurer, presence: true
 end
