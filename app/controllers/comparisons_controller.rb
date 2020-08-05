26f42f569d0fb89b56843acbb3f628606db5d1fa
Austin Mason <austinpmason@gmail.com>
2020-08-05 21:54:47 +0100

    Create comparison controller
    Used to direct requests relating to new international health insurance
    quotes

---
+++ app/controllers/comparisons_controller.rb
@@ -0,0 +1,4 @@
+class ComparisonsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   devise_for :users, controllers: { invitations: 'users/invitations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :comparisons
 end
