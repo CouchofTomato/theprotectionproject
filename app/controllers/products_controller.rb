71f46b8d54b22fc2a424b780a65a381730d359e3
Austin Mason <austinpmason@gmail.com>
2020-08-17 20:44:33 +0100

    Add Products to Product Select
    Because: I want to add an insurers products to the product select box
    when an insurer is selected on the new comparison page
    
    This commit: Sends a remote fetch request to the products controller
    sending the insurer id.
    
    The request is sent for a json response and the controller provides a
    json response of the selected insurers products. The json response is
    then converted into an array and iterated creating a new option for the
    product select box and appending it.
    
    When an insurer is selected the product select is cleared

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Insurer.find(params[:insurer_id]).products
+
+    respond_to do |format|
+      format.json { render json: @products }
+    end
+  end
 end
--- app/javascript/controllers/health_comparison_product_select_controller.js
+++ app/javascript/controllers/health_comparison_product_select_controller.js
@@ -2,10 +2,28 @@ import { Controller } from "stimulus";
 import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = ["insurer"];
+  static targets = ["insurer", "product"];
 
   getProducts(event) {
+    this.productTarget.length = 1
     if (this.insurerTarget.value == '') return;
+
+    fetch(`/insurers/${this.insurerTarget.value}/products`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => this.addProductsToSelect(data))
+  }
+
+  addProductsToSelect(products) {
+    let productSelect = this.productTarget
+
+    products.forEach((product, index) => {
+      const option = new Option(product.name, product.id)
+      productSelect.add(option, undefined)
+    })
   }
 }
 
