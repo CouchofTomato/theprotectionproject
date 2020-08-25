30942096bcf35c200c8a64646bf72849ebb8ab22
Austin Mason <austinpmason@gmail.com>
2020-08-25 21:52:16 +0100

    Add benefits to comparison table

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -1,5 +1,6 @@
 class ComparisonsController < ApplicationController
   def new
-    @benefits = Benefit.all
+    @benefits = Benefit.grouped_by_category
+    @benefit_categories = Benefit.categories.keys.select { @benefits.keys.include?(_1) }
   end
 end
