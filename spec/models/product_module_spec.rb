8127e90d6cd58c1e6a8e841bc8e29b127b4bddd0
Austin Mason <austinpmason@gmail.com>
2020-09-15 20:11:53 +0100

    Remove currency validation on product module model
    Because: A product module doesn't have to have an overall sum assured
    
    This commit: Removes the validation on the format of the sum assured for
    a product module.

--- spec/models/product_module_spec.rb
+++ spec/models/product_module_spec.rb
@@ -9,8 +9,6 @@ RSpec.describe ProductModule, type: :model do
   it { expect(product_module).to validate_uniqueness_of(:name).case_insensitive.scoped_to(:category) }
   it { expect(product_module).to validate_presence_of :category }
   it { expect(product_module).to validate_presence_of :sum_assured }
-  it { expect(product_module).to allow_value('USD 5,00 | GBP 6,00 | EUR 7,00').for(:sum_assured) }
-  it { expect(product_module).not_to allow_value('USD 5,00 | GBP 6,00 | EUR 7,00 | TNT 5,00').for(:sum_assured) }
 
   it do
     expect(product_module).to define_enum_for(:category)
