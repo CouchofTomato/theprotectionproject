0e83d20d3b2d0fd7a97e5fd90f4ee537dfb0f0fe
Austin Mason <austinpmason@gmail.com>
2020-09-30 22:02:01 +0100

    Add insurers who offer individual products to individual comparison
    Because: I want the individual comparison product select form to only
    show insurers who offer individual products
    
    This commit: Changes the insruer select input from all insurers to those
    who have individual products using the scope on Insurer for finding
    Insurers who have products with a customer type passed in as an argument
    
    The argument comes from a parameter sent in the get request to the
    new comparison path

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -2,6 +2,9 @@ class ComparisonsController < ApplicationController
   def new
     @grouped_benefits = Benefit.grouped_by_category
     @benefit_categories = Benefit.categories.keys.select { @grouped_benefits.key? _1 }
+    @insurers = Insurer
+                .offers_products_with_customer_type(params[:customer_type])
+                .order(:name)
   end
 
   def show
