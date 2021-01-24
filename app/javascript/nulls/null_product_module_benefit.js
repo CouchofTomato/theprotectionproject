0e55a81a777aa9149344990d7eac1f40ce213f20
Austin Mason <austinpmason@gmail.com>
2021-01-24 23:02:44 +0000

    Remove unnecessary Javascript associated with comparison table UI
    Because: The comparison table UI is now rendered from the backend using
    a reflex
    
    This commit: Removes any Javascript files and associated tests that were
    used to render UI for the comparison table page

--- app/javascript/nulls/null_product_module_benefit.js
+++
@@ -1,8 +0,0 @@
-class NullProductModuleBenefit {
-  constructor() {
-    this.benefit_icon =
-      "<span class='icon'><i class='fa fa-times icon--not-covered'></i></span>";
-  }
-}
-
-export default NullProductModuleBenefit;
