89e2ecfc097d5053685e754c46a65d1244632bd3
Austin Mason <austinpmason@gmail.com>
2021-02-01 21:40:50 +0000

    add redis-session-store gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,10 +112,16 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-  config.session_store :cache_store,
-                       key: '_session',
-                       compress: true,
-                       pool_size: 5,
-                       expire_after: 1.year
+  config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('REDIS_URL') }
+
+  config.session_store :redis_session_store, {
+    key: Rails.application.credentials.app_session_key,
+    serializer: :json,
+    redis: {
+      expire_after: 1.year,
+      ttl: 1.year,
+      key_prefix: 'app:session:',
+      url: ENV.fetch('HEROKU_REDIS_MAROON_URL')
+    }
+  }
 end
